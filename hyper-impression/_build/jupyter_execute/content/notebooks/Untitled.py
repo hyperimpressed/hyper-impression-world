17652597ea1667a2c9d0c2cc5f828df77f216385
atj61275 <jo.d.a@hotmail.de>
2021-05-28 00:24:25 +0200

    my first jupyter book

---
+++ hyper-impression/_build/jupyter_execute/content/notebooks/Untitled.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Durchführer
+# 
+# Es ist bereits eine Leistung, sich zeitlich und mental zu ermöglichen in den (PlanerZustand)[Link zur Page dazu] zu gelangen, in dem eigene Probleme ohne emotionale Bewertung erkannt werden und rational Lösungspläne erarbeitet werden können.
+# 
+# Jedoch ändert all das nicht das tatsächliche Leben, wenn diese Pläne nicht mit in den Alltag getragen werden. \
+# In diesem ist man gewöhnlich nicht so abgegrenzt von Emotionen, da man in Ausführungen vertieft oder Druck ausgesetzt ist.
+# 
+# *So tue ich mich schwer, Fremde Menschen anzusprechen und erkenne dies als etwas, das ich abändern möchte.\
+# Planbewusstsein und neue Interpretation darüber, dass ich ohne negative Konsequenzen in solche Interaktionen gehen kann,
+# ist zwar notwendig. \
+# Jedoch lösen sie nicht das Problem des angewohnten Gefühls und Denkprozesses in der* ***tatsächlichen Situation.***
+# 
+# 
+
+# # Arbeit: Warten
+# Ich bekomme das Gefühl, dass ich während der Zeit etwas anderes tun sollte/darf, da es sonst verschwendete Zeit ist.\
+# Ich müsste diese Zeit für das Finden der Achtsamkeit für mein Handeln verwenden.
+# #### Multitasking ist schlecht für mich!
---
+++ hyper-impression/_build/jupyter_execute/impression_tools.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+# Realisation = $\otimes$\
+# Task $= \odot$
+# 
+
+# In[60]:
+
+
+get_ipython().run_cell_magic('html', '', '<link href="https://fonts.googleapis.com/css?family=Montserrat:800,500" rel="stylesheet">')
+
+
+# In[81]:
+
+
+3+4
+
+
+# In[154]:
+
+
+
+get_ipython().run_cell_magic('html', '', '<style>\n\n:root {\n  --bg: #310151 !important;\n}\n\nbody {\n   background:;\n   font-family: \'Montserrat\';\n   font-style: bold;\n   font-weight: 500; /\n   font-size: large;\n   color: rgb(31, 28, 31);\n}\n\n\ndiv.container {\nbackground-color: #310151 !important;\nborder: 0px;\nleft: 0px;\nmargin-bottom: 0em;\nmargin-top: 0em;\nborder-top-right-radius: 0px;\nborder-top-left-radius: 0px;\n}\n\ndiv.prompt { display:none }\n\n\n\ndiv.prompt_container {\nbackground-color: #310151 !important;\nborder: 0px;\nleft: 100px;\nmargin-bottom: 0em;\nmargin-top: 0em;\nborder-top-right-radius: 0px;\nborder-top-left-radius: 0px;\n\n    \n}\n\n\ndiv.inner_cell {\nbackground-image: url("pre.jpg");\nbackground-color: #cccccc;\nborder: 0px;\nleft: 0px;\nmargin-bottom: 0em;\nmargin-top: 0em;\nborder-top-right-radius: 0px;\nborder-top-left-radius: 0px;\n}\n\n\n\n\n\nh1 {\n   font-family: \'Montserrat\';\n   font-style: bold;\n   font-weight: 800;\n   font-size: large;\n   color: rgb(31, 28, 31);\n}\n\nh2 {\n   font-family: \'Montserrat\';\n   font-style: bold;\n   font-weight: 1000;\n   font-size: xxx-large;\n    \n#myVideo {\n  position: fixed;\n  right: 0;\n  bottom: 0;\n  min-width: 100%;\n  min-height: 100%;\n}\n\n.content {\n  position: fixed;\n  bottom: 0;\n  background: rgba(0, 0, 0, 0.5);\n  color: #f1f1f1;\n  width: 100%;\n  padding: 20px;\n}\n\n</style>\n\n\n#Not needed:\n<script>\n// Get the video\nvar video = document.getElementById("myVideo");\n\n// Get the button\nvar btn = document.getElementById("myBtn");\n\n// Pause and play the video, and change the button text\nfunction myFunction() {\n  if (video.paused) {\n    video.play();\n    btn.innerHTML = "Pause";\n  } else {\n    video.pause();\n    btn.innerHTML = "Play";\n  }\n}\n</script>\n\n\n\n\n\n\n<video muted id="myVideo">\n  <source src="Test.mp4" type="video/mp4">\n</video>\n<div class="content">\n  <h1>Heading</h1>\n  <p>Lorem ipsum...</p>\n  <!-- Use a button to pause/play the video with JavaScript -->\n  <button id="myBtn" onclick="video.play()">Pause</button>\n</div>\n\n\n')
+
+
+# # Impression Tools
+# 
+# The following collection of findings is supposed to enable you to use ***knowledge about the nature of your own mind*** to your advantage, *e.g. in combating habits, regulating emotions and organising your life*.
+# 
+# ## Introduction
+# In every moment of your life, there will be 
+# 1. a certain feeling (gut) and
+# 2. a certain interpretation (mind)
+# about the world around you.
+# *Say, you leave the house in an uneasy mood and a person across the street is laughing.\
+# Your **feeling** is unease, as I said,\
+# but your **interpretation** of the world will be influenced by that:\
+# you may belief, that the persn is laughing **at** you.*
+# 
+# Here is our human problem: We only get to affect our interpretations directly. The feelings, we are at the mercy of. At least aparently ...
+# 
+# *MEHR ERKLÄRUNG NÖTIG*
+# 
+# ## 1.Ground Principles of Mind
+# 
+# ## Interpretation *of* Feelings
+# Setze den Glauben ein, dass deine Gefühle nicht die Wahrheit tatsächlich beschreiben.
+# Warum reicht das noch nicht? Es ist nötig und hilfreich für verankerte Gefühle von schlechrten Gewohnheiten, aber ein ständiges Battle, das verlangt, dass du immer conscious bleibst kann nciht endziel sein.\
+# ALso ist unser Ziel, dass wir uns Gefühle einrichten, die positiv helfen.\
+# Ziel: Über impressionen neue interpretationen emotional verankern und reinforcen.
+# 
+# ## *Deciding* on Interpretations
+# MEHR ERKLÄR \
+# Wenn jedes Gefühl wiederlegbar ist, dann auch das Gefühl, dass die derzeitige Interpretation wahr ist.
+# Somit wird die Interpretation eine völlig freie Entscheidung.
+# 
+# You can, in certain conscious moments, let go of a held interpretation and *decide* to believe in another one instead. \
+# This is especially useful, when you have held beliefs that are in conflict with facts of the real world *e.g. I am convinced this guy hates me, eventhough he does not*, or which are actually *unknowable*, while believing affects you negatively. 
+# 
+# ## 2.Methods for Impressions
+# Wie man gefühle über neu erlernte, besser geeignete Interpretationen der welt verankert, sodass man immer mehr emotional dem Bild entspricht, dass man rational erwünschen würde.
+# 
+# ## Example catching
+# Whenever you get a strong impression about a specific situation, *e.g. "I just realized, that I can do ten pushups without a problem, even though I heavily felt, I don't want to/couldn't before"*, you should not let that opportunity pass.
+# #### Sidenote
+# We generally overestimate our ability to remember. You might think, "I definitely will remember this pushup-incident, so no need to think much about it", but ponder on this/try this out yourself: \
+# **Before going to bed, think of a bunch of things that just crossed your mind. Write them down. Tomorrow morning, read them again and check, if you would have remembered, if you hadn't written them down.**
+# #### End Sidenote
+# So you realize, that a strong impression also will not stand the test of time, unless it is intently turned into a **powerful impression**, meaning, one, that will come around by itself every now and then, because it has robust foundation in your mind.
+# 
+# For this, you have to encapsulate the example to stand for the clear impression *(in this case "Pushup-incidence" stores "What I strongly feel to be impossible/undoable still can be.")*. \
+# Formulate in words for yourself exactly, what you learned and recognize, that you feel the truth of it in your body, as you have just witnessed this very thing.
+# 
+# #### Explaination
+# We learn from impressionate situations althroughout our childhood. We get a very strong sense of something, as we have witnessed a situation that indicates the truth of this interpretation. And we store this impression emotionally, mostly unquestioned, in our understanding of the world. \
+# *As a child I fall into the mud. This induces a vivid notion, that everyone around is looking at me and judging me.\
+# I feel with strong emotions, that it is happening now and therefore store the interpretation "Everyone judges me and it feels bad" as a **powerful impression**.*
+# 
+# The truth of the interpretation is not questioned, just assumed true.
+# 
+# But now, as we learn to *decide* on interpretations, we can look back and recognize, that this experiences does not mean, the interpretation we gained from it is actually describing the real world. 
+# 
+# Furthermore, we recognize our inability to ever know accurately what others think and therefore our best option is to choose an interpretation that helps us live our life.
+# 
+# #### End of Explaination
+# 
+# But the just described, rather arbitrary process *(just because I fell into the mud, I have paranoia)* can be used intently as well:\
+# I want to better my life. Therefore I think about my problems. I recognize some of them. I try to push myself to go beyond them. I experience my ability to overcome them and how I did it.\
+# This is the experience we intently turn into a **powerful impression**.
+# 
+# ## Attentive Writing
+# Aufschreiben, sodass die sätze nicht für den schreibenden, sondern den lesenden gemacht sind. Dass sie die erkenntnisse kompakt fast schon bibel artig aufführen, ohne viel eigene persönlichkeit oder momentüberzeugungen hineinzustecken.
+# 
+# 
+# ## Place and Object Ritualisation
+# ...
+# You will notice, that certain thought-patterns will arise, whenever you encounter this place. \
+# *Example*
+# But, as you are now able to choose an interpretation freely, you are enabled to think of the particular interpretations you want to connect with this place intently.
+# 
+# *I am now sitting here at my desk and I want this place to be one, where I feel free of any old habits that might want to drag me towards unhealthy actions and thoughts.*
+# 
+# You experience, what you focus your attention on and intently let this impression stick to the place as well.\
+# Such that the next time you come to this place, you will reinforce the impression again and so on, till it becomes self inducing.
+# 
+
+# 
+
+# 
+# ```{toctree}
+# :hidden:
+# :titlesonly:
+# 
+# 
+# markdown
+# ```
+# 
